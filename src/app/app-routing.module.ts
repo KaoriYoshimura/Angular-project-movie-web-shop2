c26734b2b8f371577be622e67c0e2e45d1fa300d
KaoriYoshimura <infmalbyevo@gmail.com>
2019-05-12 15:48:03 +0200

    install angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { CartComponent } from './cart/cart.component';
-// import { CheckoutComponent } from './checkout/checkout.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 // import { ConfirmComponent } from './confirm/confirm.component';
 
@@ -12,7 +12,7 @@ const appRoutes: Routes= [
   { path: '', component:HomeComponent },
   { path: 'product/:id', component:ProductComponent },
   { path: 'cart', component:CartComponent },
-  // { path: 'checkout', component:CheckoutComponent },
+  { path: 'checkout', component:CheckoutComponent },
   // { path: 'confirm', component:ConfirmComponent },
   { path: '**', component:PageNotFoundComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { MatButtonModule, MatCheckboxModule, MatFormFieldModule } from '@angular/material';
-// import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule, MatCheckboxModule, MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app-component/app.component';
-// import { ProductsComponent } from './products/products.component';
-// import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CartComponent } from './cart/cart.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-// import { CheckoutComponent } from './checkout/checkout.component';
-// import { ConfirmComponent } from './confirm/confirm.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -26,22 +22,18 @@ import { CheckoutComponent } from './checkout/checkout.component';
     CartComponent,
     PageNotFoundComponent,
     CheckoutComponent,
-    // CheckoutComponent,
-    // ConfirmComponent
-    // PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    // AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    // MatButtonModule,
-    // MatCheckboxModule,
-    // MatInputModule,
-    // MatFormFieldModule
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]

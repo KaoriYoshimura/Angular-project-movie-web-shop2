9f22aef006684b55ce34b90ff9028f86fd3165bc
KaoriYoshimura <infmalbyevo@gmail.com>
2019-05-12 11:34:54 +0200

    update app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './Home/home.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
+// import { CheckoutComponent } from './checkout/checkout.component';
+// import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+// import { ConfirmComponent } from './confirm/confirm.component';
+
+const appRoutes: Routes= [
+  { path: '', component:HomeComponent },
+  { path: 'product/:id', component:ProductComponent },
+  { path: 'cart', component:CartComponent },
+  // { path: 'checkout', component:CheckoutComponent },
+  // { path: 'confirm', component:ConfirmComponent },
+  // { path: '**', component:PageNotFoundComponent },
+];
 
 @NgModule({
-  declarations: [],
   imports: [
+  // This will make the routes appear in your application(not browser yet)
+  RouterModule.forRoot(
+    appRoutes,
+    { enableTracing: true } // <-- debugging purposes only
+    ),
     CommonModule
-  ]
+  ],
+  // Exporting RouterModule makes AppModule component available which needs router directives.
+  exports: [ RouterModule ],
+  declarations: []
 })
 export class AppRoutingModule { }

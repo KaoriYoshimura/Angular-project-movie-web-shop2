e185bf5f40b8bc7dc19f2c68e6c8b78d9c52f32c
KaoriYoshimura <infmalbyevo@gmail.com>
2019-05-12 15:44:15 +0200

    add form for userinformation

--- src/app/checkout/checkout.component.spec.ts
+++ src/app/checkout/checkout.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CheckoutComponent } from './checkout.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatInputModule, MatFormFieldModule } from '@angular/material';
+import { DataService } from '../services/data.service';
+import { MockDataService } from '../services/mock-data.service';
 
 describe('CheckoutComponent', () => {
   let component: CheckoutComponent;
@@ -8,7 +15,24 @@ describe('CheckoutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CheckoutComponent ]
+      declarations: [ CheckoutComponent ],
+      imports: [HttpClientModule,
+        RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatInputModule,
+        MatFormFieldModule]
+    })
+    //Override component's own provider
+    .overrideComponent(CheckoutComponent, {
+      set: {
+        providers: [
+          { provide: DataService, useClass: MockDataService }
+        ]
+      }
     })
     .compileComponents();
   }));
@@ -22,4 +46,65 @@ describe('CheckoutComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be invalid when empty', () => {
+    expect(component.userForm.valid).toBeFalsy();
+  });
+
+  it('should validate email field', () => {
+    let itemfirstName = component.userForm.controls["firstName"];
+    itemfirstName.setValue("Media");
+    let itemLasttName = component.userForm.controls["lastName"];
+    itemLasttName.setValue("institutet");
+    let itemEmailName = component.userForm.controls["email"];
+    itemEmailName.setValue("Media@institutet.com");
+    let itemConfirmEmail = component.userForm.controls["confirmEmail"];
+    itemConfirmEmail.setValue("Media@institutet.com");
+    let itempaymentMethod = component.userForm.controls["paymentMethod"];
+    itempaymentMethod.setValue("Paypal");
+    let itemStreet = component.userForm.controls["street"];
+    itemStreet.setValue("Tulegatan 41");
+    let itemCity = component.userForm.controls["city"];
+    itemCity.setValue("Stockholm");
+    let itemPostCode = component.userForm.controls["postcode"];
+    itemPostCode.setValue("12345");
+    let itemphoneNumber = component.userForm.controls["phoneNumber"];
+    itemphoneNumber.setValue("1234567");
+
+    expect(component.userForm.valid).toBeTruthy();
+    expect(component.userForm.controls["firstName"].valid).toBeTruthy;
+    expect(component.userForm.controls["lastName"].valid).toBeTruthy;
+    expect(component.userForm.controls["postcode"].valid).toBeTruthy;
+    expect(component.userForm.controls["phoneNumber"].valid).toBeTruthy;
+  });
+
+  // it(`Should add a new phone number field`, () => {
+  //   // const fb = new FormBuilder();
+  //   // let phoneNumbers = this.fb.array([
+  //   //   this.fb.control('')
+  //   // ]);
+  //   const fb = new FormBuilder();
+  //   const myObject = fb.group('myArray': fb.array());
+    
+  //   expect(this.phoneNumbers).toBeUndefined();
+    
+  //   component.userForm.get('phoneNumbers')['controls'][0];
+
+
+  //   // const arrayLength = component.userForm.get('phoneNumbers')[0];
+  //   // const arrayLength = component.userForm.get('phoneNumbers')['controls'][0].length;
+  //   // component.userForm.get('phoneNumbers')[0];
+  //   component.addPhoneNumber();
+  //   // expect(arrayLength).toBeTruthy();
+  //   expect(this.phoneNumbers.length).toBe(1);
+
+  // });
+
+  it(`Should caluculate total cost`, () => {
+    expect(component.cartItems.length).toBe(2);
+    // caluculateCost is in ngOnInit so writing component.caluculateCost() is not necessary;
+    expect(component.totalCost).toBe(328);
+
+  });
+
 });
--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,15 +1,82 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
+import { IProduct } from '../interfaces/iproduct';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent implements OnInit{
+  cartItems: IProduct[];
+  totalCost = 0;
 
-  constructor() { }
+  // Inject FormBuilder service
+  constructor(
+    private fb: FormBuilder,
+    private service: DataService
+  ) { }
 
-  ngOnInit() {
+
+  userForm = this.fb.group({
+    firstName: ['', [Validators.required, Validators.minLength(2)]],
+    lastName: ['', [Validators.required, Validators.minLength(2)]],
+    email: ['', Validators.required],
+    confirmEmail: ['', Validators.required],
+    paymentMethod: ['', Validators.required],
+    street: ['', Validators.required],
+    street2: [''],
+    city: ['', Validators.required],
+    postcode: ['', [Validators.required, Validators.minLength(5)]],
+    phoneNumber: ['', [Validators.required, Validators.minLength(7)]],
+    phoneNumbers: this.fb.array([
+      this.fb.control('')
+    ])
+  },{Validator: matchingFields('password', 'confirmEmail')});
+
+  get phoneNumbers() {
+    return this.userForm.get('phoneNumbers') as FormArray;
   }
 
+  addPhoneNumber() {
+    this.phoneNumbers.push(
+      this.fb.control('')
+      );
+      console.log(this.phoneNumbers);
+  }
+
+  removePhoneNumber(i: number) {
+    this.phoneNumbers.removeAt(i);
+}
+
+  billingOnSubmit(){
+    // console.log(this.userForm.value);
+    sessionStorage.setItem('userData', JSON.stringify(this.userForm.value));
+
+  }
+
+
+  ngOnInit() {
+    this.getCartItems();
+    // this.caluculateCost();
+  }
+
+  getCartItems(){
+    this.cartItems = this.service.getSessionCartItems();
+    this.caluculateCost();
+  }
+
+  caluculateCost(){
+    this.totalCost = this.service.caluculateTotalCost();
+}
+
+}
+
+function matchingFields(field1, field2){
+  console.log('works')
+  return form => {
+    if(form.controls[field1].value !== form.controls[field2].value)
+     return {matchingFields: true}
+  }
 }

bcfec56d57bc957212b6e84f8c6527aaf35d5401
KaoriYoshimura <infmalbyevo@gmail.com>
2019-05-12 11:47:10 +0200

    update app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './Home/home.component';
+import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { CartComponent } from './cart/cart.component';
 // import { CheckoutComponent } from './checkout/checkout.component';
@@ -11,6 +11,7 @@ import { CartComponent } from './cart/cart.component';
 const appRoutes: Routes= [
   { path: '', component:HomeComponent },
   { path: 'product/:id', component:ProductComponent },
+  { path: 'product', component:ProductComponent },
   { path: 'cart', component:CartComponent },
   // { path: 'checkout', component:CheckoutComponent },
   // { path: 'confirm', component:ConfirmComponent },

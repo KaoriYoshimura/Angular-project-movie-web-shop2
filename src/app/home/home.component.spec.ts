6c7f7f22705e2817af0c54d0ed7f08fb96f8a856
KaoriYoshimura <infmalbyevo@gmail.com>
2019-05-12 15:27:24 +0200

    add a list of products in home component

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,17 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { DataService } from '../services/data.service';
+import { MockDataService } from '../services/mock-data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('HomeComponent', () => {
+describe('ProductsComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  // Call testBed methods within a beforeEach() to ensure a fresh start before each indivisual test
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule],
       declarations: [ HomeComponent ]
     })
+
+    //Override component's own provider
+    .overrideComponent(HomeComponent, {
+      set: {
+        providers: [
+          { provide: DataService, useClass: MockDataService }
+        ]
+      }
+    })
     .compileComponents();
-  }));
+    }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
@@ -22,4 +37,8 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain 2 products',() => {
+    expect(component.products.length).toBe(2);
+  })
 });
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../interfaces/iproduct';
+// import { MockDataService } from '../services/mock-data.service';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  products: IProduct[];
 
-  constructor() { }
+  // Inject the class of DataService
+  constructor(private service:DataService) { }
 
   ngOnInit() {
+    this.service.getData().subscribe((data) => {
+      this.products = data;
+    });
   }
 
 }

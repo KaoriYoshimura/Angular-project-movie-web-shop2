87664dfcfaf643fd16bccc484440f68fe34437cf
KaoriYoshimura <infmalbyevo@gmail.com>
2019-05-14 14:34:07 +0200

    update and run unit test

--- src/app/order-sent/order-sent.component.spec.ts
+++ src/app/order-sent/order-sent.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrderSentComponent } from './order-sent.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from '../services/data.service';
+import { MockDataService } from '../services/mock-data.service';
 
 describe('OrderSentComponent', () => {
   let component: OrderSentComponent;
@@ -8,7 +12,16 @@ describe('OrderSentComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OrderSentComponent ]
+      declarations: [ OrderSentComponent ],
+      imports: [HttpClientModule, RouterTestingModule]
+    })
+    //Override component's own provider to test with MockData.service
+    .overrideComponent(OrderSentComponent, {
+      set: {
+        providers: [
+          { provide: DataService, useClass: MockDataService }
+        ]
+      }
     })
     .compileComponents();
   }));

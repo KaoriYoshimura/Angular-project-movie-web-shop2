a272b39c3d712c40801ee11032561adb5165660a
KaoriYoshimura <infmalbyevo@gmail.com>
2019-05-18 08:53:55 +0200

    update search and category style

--- src/app/app-component/app.component.ts
+++ src/app/app-component/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { DataService } from '../services/data.service';
+import { IProduct } from '../interfaces/iproduct';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-webshop-app';
   faBars = faBars;
+  NumberOfCartItems : IProduct[];
+
+  constructor(
+    private service: DataService
+  ){
+  }
+
+  ngOnInit(){
+  }
+
 }

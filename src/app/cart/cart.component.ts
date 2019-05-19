e45511c39dedc2e7a3dd07b097cfb13dade91201
KaoriYoshimura <infmalbyevo@gmail.com>
2019-05-19 06:36:54 +0200

    update style for cart page

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../interfaces/iproduct';
 import { DataService } from '../services/data.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
@@ -10,19 +11,40 @@ import { DataService } from '../services/data.service';
 export class CartComponent implements OnInit {
   
   cartItems: IProduct[];
+  NumberOfCartItems: number;
+  isCartItems = this.NumberOfCartItems > 0;
 
-  constructor(private service:DataService) { }
+  constructor(
+    private service:DataService,
+    private location: Location,
+    ) { }
 
   ngOnInit() {
     this.getCartItems();
+    console.log(this.isCartItems);
+    console.log(this.NumberOfCartItems);
+
   }
 
   getCartItems(){
     this.cartItems = this.service.getSessionCartItems();
+    this.countNumberOfCartItems();
+  }
+
+  countNumberOfCartItems() {
+    this.NumberOfCartItems = this.cartItems.length;
+
+    return this.NumberOfCartItems;
   }
 
   removeFromCart(item: number) {
     this.service.RemoveFromSessionStorage(item);
+    // location.reload(true);
+  }
+
+  // Back to previous page
+  goBack(): void {
+    this.location.back();
   }
 
 }

7bb70688e038499243a65e772fd06dbc4b675a1a
KaoriYoshimura <infmalbyevo@gmail.com>
2019-05-20 11:19:15 +0200

    add all functions to IDataService

--- src/app/interfaces/idata-service.ts
+++ src/app/interfaces/idata-service.ts
@@ -1,6 +1,23 @@
 import { Observable } from 'rxjs';
 import { IProduct } from './iproduct'
+import { IPlacedOrders } from './iplaced-orders';
+import { IOrder } from './iorder';
+import { ICategory } from './icategory';
 
 export interface IdataService {
     getData():Observable<IProduct[]>;
+    getDetailById(id: number): Observable<IProduct>;
+    getSessionCartItems():void;
+    countNumberOfCartItems():void;
+    addToCart(details: IProduct):void;
+    RemoveFromSessionStorage(item: number):void;
+    getOrders(): Observable<IPlacedOrders[]>;
+    caluculateTotalCost():void;
+    getSessionUserData():void;
+    // submitOrder(order: IOrder): Observable<IOrder>;
+    searchProductApi(Query: string):Observable<IProduct[]>;
+    getCategory():Observable<ICategory[]>;
+    getOrders(): Observable<IPlacedOrders[]>;
+    // updateOrders(id:number, updateOrder:IPlacedOrders): Observable<IPlacedOrders>;
+    deleteOrderRow(id:number): Observable<IPlacedOrders>;
 }

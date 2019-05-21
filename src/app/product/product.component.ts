5b21eb60b79f59e37c2b42b841c915cbd1fcd57d
KaoriYoshimura <infmalbyevo@gmail.com>
2019-05-21 18:21:00 +0200

    add category and update style for product page

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { IProduct } from '../interfaces/iproduct';
-import { DataService } from '../services/data.service';
+import { Router } from '@angular/router';
+import { ICategory } from '../interfaces/icategory';
 
 @Component({
   selector: 'app-details',
@@ -12,12 +14,15 @@ import { DataService } from '../services/data.service';
 export class ProductComponent implements OnInit {
   details: IProduct;
   NumberOfCartItems : number;
+  categories: ICategory[];
+  categoryResults: ICategory[] = [];
 
   // Implement ActivatedRoute to use dependency injection
   constructor(
     private route: ActivatedRoute,
-    private location: Location,
-    private service: DataService
+    private service: DataService,
+    private router: Router,
+    private location: Location
   ) { }
 
   // Get the property params from product.component, 'id' and copies the data into myParams. Use this id to collect the item with the same id from API.
@@ -25,25 +30,43 @@ export class ProductComponent implements OnInit {
     this.route.params.subscribe(myParams => {
       const id = myParams['id'];
       this.getMovie(id);
-      this.NumberOfCartItems = this.service.countNumberOfCartItems();
     });
-
   }
 
   // Create function separately in order to test even it works within ngOnInit
-  getMovie(id: number){
+  getMovie(id: number) {
     this.service.getDetailById(id).subscribe((detailsFromApi)=> {
       this.details = detailsFromApi;
+      this.findCcategory();
     });
   }
 
   // Back to previous page
-  goBack(): void {
+  goBack(): void{
     this.location.back();
   }
 
-  addToCart(): void {
+  addToCart(): void{
     this.service.addToCart(this.details);
-    this.NumberOfCartItems = this.service.countNumberOfCartItems();
+    this.router.navigate(['/cart']);
   }
+
+  findCcategory(){
+    this.service.getCategory().subscribe(
+      response => {
+        this.categories = response;
+        for(var i=0; i<this.categories.length; i++){
+          for(var j=0; j<this.details.productCategory.length; j++){
+            if(this.categories[i].id === this.details.productCategory[j].categoryId){
+              this.categoryResults.push(this.categories[i]);
+            }
+          }
+        }
+        console.log(this.categoryResults);
+      },
+      error => console.log(error),
+      () => console.log('HPPT request for category completed')
+    );
+  }
+
 }

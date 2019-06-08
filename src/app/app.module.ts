992bbbf892cb71b8a4a02e46153e9091a35acd88
KaoriYoshimura <infmalbyevo@gmail.com>
2019-06-08 22:41:11 +0200

    delete commented out code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { /* MatButtonModule, MatCheckboxModule, MatFormFieldModule,  */MatPaginatorModule, MatTreeModule } from '@angular/material';
+import { MatPaginatorModule, MatTreeModule } from '@angular/material';
 import { MatTableModule } from '@angular/material/table';
-// import { MatInputModule } from '@angular/material/input';
 import { CdkTableModule } from '@angular/cdk/table';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbModule, /* NgbActiveModal */ } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app-component/app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CartComponent } from './cart/cart.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
-import { ProductComponent,/*  NgbdModalContent */ } from './product/product.component';
+import { ProductComponent } from './product/product.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { ConfirmComponent } from './confirm/confirm.component';
@@ -49,18 +48,12 @@ import { notifyModalContent } from './notify-dialog/notify-dialog.component';
     FormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    // MatButtonModule,
-    // MatCheckboxModule,
-    // MatInputModule,
-    // MatFormFieldModule,
     MatTableModule,
     MatPaginatorModule,
-    // MatDialogModule,
     CdkTableModule,
     MatTreeModule,
     FontAwesomeModule,
     NgbModule,
-    // NgbActiveModal
   ],
   entryComponents: [notifyModalContent],
   providers: [],
